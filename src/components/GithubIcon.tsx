d56c4ee8e337270c679bbe7c39f28eb701ea190e
Carlos Alvarez <carlosalvarez1391@gmail.com>
2025-03-21 03:51:03 +0100

    feat: cleanup

--- src/components/GithubIcon.tsx
+++ src/components/GithubIcon.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 const GithubIcon = () => {
     return (
         <svg width="25px" height="25px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
--- src/components/LinkedinIcon.tsx
+++ src/components/LinkedinIcon.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 const LinkedInIcon = () => {
     return (
         <svg fill="#000000" height="25px" width="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
